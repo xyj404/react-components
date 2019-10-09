adb1f4605a0ce98cbfdd3c5727c93cc654a518b2
xyj <593500664@qq.com>
2019-10-09 14:11:14 +0800

    change to test git config

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> an save to reload.
+          Edit <code>src/App.tsx</code> an ave to reload.
         </p>
         <a
           className="App-link"
